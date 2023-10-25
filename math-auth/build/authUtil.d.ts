7a456a8fa9339e3a4c2598d25531e2338df40b83
hanantomer <hanantomer@gmail.com>
2023-10-25 22:11:42 +0300

    Delete math-auth/build directory

--- math-auth/build/authUtil.d.ts
+++
@@ -1,7 +0,0 @@
-import User from "../../math-db/build/models/user.model";
-import { UserAttributes } from "../../math-common/build/userTypes";
-export default function useAuthUtils(): {
-    authByLocalPassword: (email: string, password: string) => Promise<UserAttributes | null>;
-    authByGoogleToken: (access_token: string) => Promise<User | null | undefined>;
-    authByLocalToken: (access_token: string) => Promise<UserAttributes | undefined>;
-};
