defe83ce8fec46e86c5e56fcc868f3c1919a954a
Hanan Tomer <hanantomer@gmail.com>
2025-06-30 19:14:46 +0300

    Refactor: Replace VueGtag with createGtag for improved Google Analytics integration

--- math-ui3/src/plugins/index.ts
+++ math-ui3/src/plugins/index.ts
@@ -4,7 +4,7 @@ import { loadFonts } from "./webfontloader";
 import vuetify from "./vuetify";
 import store from "../store/pinia/indexStore";
 import router from "../router";
-import VueGtag from "vue-gtag";
+import { createGtag } from "vue-gtag";
 
 // Types
 import type { App } from "vue";
@@ -12,13 +12,10 @@ import type { App } from "vue";
 export function registerPlugins(app: App) {
   loadFonts();
   app.use(store).use(vuetify).use(router);
-  app.use(
-    VueGtag,
-    {
-      config: { id: import.meta.env.VITE_GA_MEASUREMENT_ID },
-      appName: "Mathboard",
-      pageTrackerScreenviewEnabled: true,
-    },
-    router
-  );
+
+  const gtag = createGtag({
+    tagId: import.meta.env.VITE_GA_MEASUREMENT_ID,
+    appName: "Mathboard",
+  });
+  app.use(gtag);
 }
