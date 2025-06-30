ebab26bc3bc55e7274330137a09aba621ba49035
Hanan Tomer <hanantomer@gmail.com>
2025-06-30 18:58:48 +0300

    Add Google Analytics tracking using vue-gtag for improved analytics

--- math-ui3/src/plugins/index.ts
+++ math-ui3/src/plugins/index.ts
@@ -4,6 +4,7 @@ import { loadFonts } from "./webfontloader";
 import vuetify from "./vuetify";
 import store from "../store/pinia/indexStore";
 import router from "../router";
+import VueGtag from "vue-gtag";
 
 // Types
 import type { App } from "vue";
@@ -11,4 +12,13 @@ import type { App } from "vue";
 export function registerPlugins(app: App) {
   loadFonts();
   app.use(store).use(vuetify).use(router);
+  app.use(
+    VueGtag,
+    {
+      config: { id: import.meta.env.VITE_GA_MEASUREMENT_ID },
+      appName: "Mathboard",
+      pageTrackerScreenviewEnabled: true,
+    },
+    router
+  );
 }
